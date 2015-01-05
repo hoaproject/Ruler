08091a6a1bf064e8eeee7972e1fadd2cae2290f4
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-01-05 14:47:59 +0100

    Happy new year!

--- DynamicCallable.php
+++ DynamicCallable.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2014, Ivan Enderlin. All rights reserved.
+ * Copyright © 2007-2015, Ivan Enderlin. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -45,7 +45,7 @@ use Hoa\Core;
  *
  * @author     Stéphane Py <stephane.py@hoa-project.net>
  * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright © 2007-2014 Stéphane Py, Ivan Enderlin.
+ * @copyright  Copyright © 2007-2015 Stéphane Py, Ivan Enderlin.
  * @license    New BSD License
  */
 
--- Exception/Exception.php
+++ Exception/Exception.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2014, Ivan Enderlin. All rights reserved.
+ * Copyright © 2007-2015, Ivan Enderlin. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -44,7 +44,7 @@ use Hoa\Core;
  * Extending the \Hoa\Core\Exception class.
  *
  * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright © 2007-2014 Ivan Enderlin.
+ * @copyright  Copyright © 2007-2015 Ivan Enderlin.
  * @license    New BSD License
  */
 
