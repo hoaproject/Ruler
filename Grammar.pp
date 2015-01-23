4e30ff948bb41bac5cd9ca75fffa88584d211cb0
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-01-23 23:07:42 +0100

    Allow an identifier to start by a digit.

--- Grammar.pp
+++ Grammar.pp
@@ -66,7 +66,7 @@
 %token  comma          ,
 %token  dot           \.
 
-%token  identifier    [^\s\(\)\[\],\.]+
+%token  identifier    (\d+[^\d\s\(\)\[\],\.]+|[^\s\(\)\[\],\.]+)
 
 #expression:
     logical_operation()
